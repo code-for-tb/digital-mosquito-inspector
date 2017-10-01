001386d55f07f0c9fac2e040e72ca73b22eeb99a
khanthulhu <johnlrbrock@gmail.com>
2017-10-01 19:56:31 -0400

    added users. moved navbar to a partial. users can sign up and log in. Added user factory for testing.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'static_pages#checklist'
 
-  resources :users
+  resources :users, except: [:destroy]
 
   get 'static_pages/checklist'
   get 'static_pages/moreInfo', as: 'moreInfo'
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence :email do |n|
+      "foo#{n}@bar.com"
+    end
+    password 'foobar'
   end
 end
