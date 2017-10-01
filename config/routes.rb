584c7234a1fa0614e91a07a476c04862a621f484
khanthulhu <johnlrbrock@gmail.com>
2017-10-01 17:45:04 -0400

    added users and divise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'static_pages#checklist'
+
+  resources :users
+
   get 'static_pages/checklist'
   get 'static_pages/moreInfo', as: 'moreInfo'
 end
